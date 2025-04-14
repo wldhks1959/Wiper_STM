62606f97eecad2abb4c95e68fdf40ca7c80d3617
jun-yeong1 <dlwnsdud1320@gmail.com>
2025-04-14 16:49:17 +0900

    test

--- Wiper_MultiTasking/Core/Src/main.c
+++ Wiper_MultiTasking/Core/Src/main.c
@@ -13,7 +13,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+ // test
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
