1f36f8106db0e0e7a4cb59b971f3d3baab043bdd
jiwan <gg812056@gmail.com>
2025-04-15 17:07:50 +0900

    pa0/1 -> pc4/5 fetch

--- Wiper_MultiTasking/Core/Inc/sensors.h
+++ Wiper_MultiTasking/Core/Inc/sensors.h
@@ -3,13 +3,12 @@
 
 #include "stm32f4xx_hal.h"
 
-uint8_t ReadDHT11(uint8_t *temperature, uint8_t *humidity); // DHT11
 uint16_t ReadCDS(void); // CDS (조도센서)
 void SetSensorLogCallback(void (*callback)(const char* msg));
-uint8_t ReadDHT22_Debug(uint8_t *temperature, uint8_t *humidity);
+// uint8_t ReadDHT11(uint8_t *temperature, uint8_t *humidity); // DHT11
+// uint8_t ReadDHT22_Debug(uint8_t *temperature, uint8_t *humidity);
 
-// bluetooth
-void Bluetooth_SendString(const char* str);
+// void Bluetooth_SendString(const char* str);
 
 void Sensors_Init(void);  // 타이머 등 필요한 초기화
 
