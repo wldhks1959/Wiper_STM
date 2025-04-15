989096858047d58047022b3e675010df5c341a13
jiwan <gg812056@gmail.com>
2025-04-15 17:40:58 +0900

    modified the jetson_spi.py

---
+++ Wiper_MultiTasking/Core/Inc/cds.h
@@ -0,0 +1,16 @@
+/*
+ * cds.h
+ *
+ *  Created on: Apr 15, 2025
+ *      Author: gg065
+ */
+
+#ifndef INC_CDS_H_
+#define INC_CDS_H_
+
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
+
+uint16_t ReadCDS(void);
+
+#endif /* INC_CDS_H_ */
